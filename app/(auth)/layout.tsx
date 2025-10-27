788af77dc1a0ff37268360d89710c0325e9f6b4d
MFajru <fajru234@gmail.com>
2025-10-28 01:34:44 +0700

    feat:responsive login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,16 +3,19 @@ import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-[#FAFAFA] w-screen h-screen flex justify-center items-center">
-      <div className="flex flex-col gap-6">
-        <Image
-          src="/rakamin.png"
-          alt="rakamin logo"
-          width={145}
-          height={50}
-          loading="eager"
-        />
-        <div className="bg-white shadow-sm w-[500px] h-fit p-10 gap-4 flex flex-col">
+    <div className="bg-[#FAFAFA] w-screen h-screen ">
+      <div className="flex flex-col gap-6 w-full h-full justify-center items-center">
+        <div className="w-[350px] sm:w-[500px] ">
+          <Image
+            src="/rakamin.png"
+            alt="rakamin logo"
+            width={145}
+            height={50}
+            loading="eager"
+          />
+        </div>
+
+        <div className="bg-white shadow-sm w-[350px] sm:w-[500px]  h-fit p-10 gap-4 flex flex-col">
           {children}
         </div>
       </div>
