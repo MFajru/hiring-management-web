6694b00865282255a7eb431817f5f8b5cbaa9143
Muhammad Fajru <fajru234@gmail.com>
2025-12-30 14:49:06 +0700

    feat: logout button

--- components/customUI/topbar.tsx
+++ components/customUI/topbar.tsx
@@ -10,6 +10,10 @@ import {
 import { IBreadcrumb } from "@/types";
 import { useTopbar } from "@/context/topbarContext";
 import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface ITopbar {
   avatarSrc?: string;
@@ -18,6 +22,11 @@ interface ITopbar {
 
 const Topbar = ({ avatarSrc: src, avatarFallback: fallback }: ITopbar) => {
   const { type, breadcrumb, title } = useTopbar();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    router.replace("/");
+  };
 
   return (
     <div className="sticky top-0 left-0 w-full h-16 flex justify-between px-5 items-center border-b border-gray-300 bg-white shadow-sm">
@@ -42,14 +51,31 @@ const Topbar = ({ avatarSrc: src, avatarFallback: fallback }: ITopbar) => {
           </Breadcrumb>
         </>
       )}
-
-      <Avatar>
-        <AvatarImage
-          src={src ? src : "https://github.com/shadcn.png"}
-          alt="@shadcn"
-        />
-        <AvatarFallback>{fallback ? fallback : "AN"}</AvatarFallback>
-      </Avatar>
+      <Popover>
+        <PopoverTrigger>
+          <Avatar className="cursor-pointer">
+            <AvatarImage
+              src={src ? src : "https://github.com/shadcn.png"}
+              alt="@shadcn"
+            />
+            <AvatarFallback>{fallback ? fallback : "AN"}</AvatarFallback>
+          </Avatar>
+        </PopoverTrigger>
+        <PopoverContent className="mr-4">
+          <div className="flex flex-col">
+            <Button
+              onClick={() => handleLogout()}
+              variant={"ghost"}
+              className="justify-start text-left text-red-500 hover:text-red-500"
+            >
+              <div className="flex gap-2 items-center">
+                <LogOut />
+                <p>Logout</p>
+              </div>
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
